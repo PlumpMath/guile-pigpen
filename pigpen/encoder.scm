57b4f9dc0768bb3c18a52f0f9acd8494ca412353
Artyom V. Poptsov <poptsov.artyom@gmail.com>
2014-12-14 21:06:31 +0400

    encoder.scm: Add copyright and comments

--- pigpen/encoder.scm
+++ pigpen/encoder.scm
@@ -1,3 +1,28 @@
+;;; encoder.scm -- Pigpen encoder.
+
+;; Copyright (C) 2014 Artyom V. Poptsov <poptsov.artyom@gmail.com>
+;;
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+;;
+;; The program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with the program.  If not, see <http://www.gnu.org/licenses/>.
+
+
+;;; Commentary:
+
+;; TODO
+
+
+;;; Code:
+
 (define-module (pigpen encoder)
   #:use-module (ice-9 optargs)
   #:use-module (ice-9 regex)
@@ -47,3 +72,5 @@
     ;; (display pigpen-list)
     ;; (newline)
     (display/pigpen pigpen-list)))
+
+;;; encoder.scm ends here
