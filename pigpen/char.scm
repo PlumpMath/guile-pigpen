cafc1dc906417b386cc481b47e1264895e782fb0
Artyom V. Poptsov <poptsov.artyom@gmail.com>
2014-12-27 20:55:21 +0300

    char.scm (<pigpen-char>): Add cipher name field
    * pigpen/char.scm (<pigpen-char>): Add cipher name field.  All related
      procedures updated.
      (pigpen-char->string, pigpen-char->list, pigpen-char->char): Update.
      (make-pigpen-char): Take a cipher name as the first argument.  All
      callers updated.
      (char->pigpen-char, list->pigpen-char): Update.

--- pigpen/char.scm
+++ pigpen/char.scm
@@ -43,16 +43,17 @@
 
 
 (define <pigpen-char>
-  (make-vtable "prpr"
+  (make-vtable "prprpr"
                (lambda (struct port)
-                 (format port "#<pigpen-char ~@c ~a>"
-                         (struct-ref struct 0)
+                 (format port "#<pigpen-char ~a ~@c ~a>"
+                         (struct-ref struct 0) ;cipher-name
+                         (struct-ref struct 1) ;char
                          (number->string (object-address struct) 16)))))
 
-(define (make-pigpen-char ch lst)
+(define (make-pigpen-char cipher-name ch lst)
   "Make a pigpen char that represents a char CH based on a list LST of
 strings."
-  (make-struct/no-tail <pigpen-char> ch lst))
+  (make-struct/no-tail <pigpen-char> cipher-name ch lst))
 
 (define (pigpen-char? x)
   "Check if X is a <pigpen-char> instance."
@@ -68,37 +69,40 @@ strings."
 
 (define (char->pigpen-char ch cipher)
   "Convert a char CH to the corresponding pigpen symbol."
-  (cond ((not (char? ch))
-         (error "Wrong type (expecting char)" ch))
-        ((char=? ch #\nul)
-         (make-pigpen-char ch (list "" "" "")))
-        ((assoc-ref (get-cipher-alphabet cipher) (char-downcase ch)) =>
-         (cut make-pigpen-char ch <>))
-        (else
-         (make-pigpen-char ch
-                           (list
-                            "     "
-                            (string-append "  " (string ch) "  ")
-                            "     ")))))
+  (let ((cipher-name (get-cipher-name cipher)))
+    (cond ((not (char? ch))
+           (error "Wrong type (expecting char)" ch))
+          ((char=? ch #\nul)
+           (make-pigpen-char cipher-name ch (list "" "" "")))
+          ((assoc-ref (get-cipher-alphabet cipher) (char-downcase ch)) =>
+           (cut make-pigpen-char cipher-name ch <>))
+          (else
+           (make-pigpen-char cipher-name
+                             ch
+                             (list
+                              "     "
+                              (string-append "  " (string ch) "  ")
+                              "     "))))))
 
 (define (pigpen-char->char pch)
   "Convert a pigpen char PCH to the corresponding character."
   (if (pigpen-char? pch)
-      (struct-ref pch 0)
+      (struct-ref pch 1)
       (error "Wrong type (expecting pigpen char)" pch)))
 
 
 (define (pigpen-char->list pch)
   "Convert a pigpen char PCH to a list of strings."
   (if (pigpen-char? pch)
-      (struct-ref pch 1)
+      (struct-ref pch 2)
       (error "Wrong type (expecting pigpen char)" pch)))
 
 (define (list->pigpen-char lst cipher)
   "Convert list of strings LST to a pigpen character using CIPHER.
 LST is expected to be a list of 3 elements."
   (if (= (length lst) 3)
-      (let* ((alphabet (get-cipher-alphabet cipher))
+      (let* ((alphabet    (get-cipher-alphabet cipher))
+             (cipher-name (get-cipher-name     cipher))
              (m (find (lambda (e)
                        (let ((l (cdr e)))
                          (and (string=? (list-ref l 0) (list-ref lst 0))
@@ -106,17 +110,17 @@ LST is expected to be a list of 3 elements."
                               (string=? (list-ref l 2) (list-ref lst 2)))))
                      alphabet)))
         (if m
-            (make-pigpen-char (car m) lst)
+            (make-pigpen-char cipher-name (car m) lst)
             (let ((ch (string-trim (list-ref lst 1))))
               (if (not (string-null? ch))
-                  (make-pigpen-char (string-ref ch 0) lst)
+                  (make-pigpen-char cipher-name (string-ref ch 0) lst)
                   (error "Could not convert the list" lst)))))
       (error "Wrong number of lines (expecting 3)" lst)))
 
 
 (define (pigpen-char->string pch)
   (if (pigpen-char? pch)
-      (let ((pchar-list (struct-ref pch 1)))
+      (let ((pchar-list (struct-ref pch 2)))
         (string-append (list-ref pchar-list 0) "\n"
                        (list-ref pchar-list 1) "\n"
                        (list-ref pchar-list 2)))
